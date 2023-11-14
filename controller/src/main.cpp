0264994abdfdd86cc7f54539729c6debc7d3a279
sixthsurge <surgecrafter@gmail.com>
2023-11-14 11:57:33 +0000

    test commit

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
+
 void setup() {
+    // this is an important comment, it would be a shame if it were to cause a merge conflict
     // write your initialization code here
 }
 
