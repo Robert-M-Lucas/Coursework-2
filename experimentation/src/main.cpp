961ba69c62bcf44c4a96f598e189034442f2c58d
robert-m-lucas <noreply@example.com>
2023-11-15 12:39:36 +0000

    Experimentation

--- experimentation/src/main.cpp
+++ experimentation/src/main.cpp
@@ -1,30 +1,13 @@
 #include <Arduino.h>
 
-unsigned long last = 0;
-
 void setup() {
     Serial.begin(9600);
-    pinMode(A0, INPUT);
-
-    last = millis();
 }
 
-unsigned long interval = 10;
+int i = 0;
 
 void loop() {
-    auto value = 0;
-
-    for (int i = 0; i < 100; i++) {
-        value = max(value, analogRead(A0));
-    }
-
-    if (value != 0) {
-        Serial.println(value);
-    }
-
-    auto current = millis();
-    if (current - last < interval) {
-        delay(interval - (millis() - last));
-    }
-    last = current;
+    Serial.println(i);
+    i++;
+    delay(500);
 }
