190c3629710144cf88530627175ef3e06db33e24
sixthsurge <surgecrafter@gmail.com>
2023-11-17 15:53:45 +0000

    2-way communication test

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
+#include <Wire.h>
+
 void setup() {
-// write your initialization code here
+    Wire.begin();
+    Serial.begin(9600);
 }
 
 void loop() {
+    // write
 
+    Wire.beginTransmission(8);
+    Wire.write("hello Rob");
+    Wire.endTransmission();
+
+    delay(500);
+
+    // read
+
+    Wire.requestFrom(8, 6);
+
+    while (Wire.available()) {
+        char c = static_cast<char>(Wire.read());
+        Serial.print(c);
+    }
+
+    delay(500);
 }
