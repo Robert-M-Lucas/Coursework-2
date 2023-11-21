220742f797f83174f8ade172b4e2f8df9b9c5111
lachlanhBath <lh2494@bath.ac.uk>
2023-11-21 11:28:04 +0000

    defined an MCP3008 device "inputAdc" on pins 13,11,12,10 for sck, mosi, miso, cs respectively

--- keyboard/src/main.cpp
+++ keyboard/src/main.cpp
@@ -5,10 +5,13 @@
 #include <Adafruit_MCP3008.h>
 
 DebugActor actor = DebugActor();
+Adafruit_MCP3008 inputAdc;
 
 void setup() {
+
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
+    inputAdc.begin(13,11,12,10);
     Serial.println("Init");
     CommunicationActor::initialise(Instrument::Keyboard, &actor);
 }
