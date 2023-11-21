9134f2e31e0e39c120d599a3cd7ee121fc1d13e3
lachlanhBath <lh2494@bath.ac.uk>
2023-11-21 11:56:26 +0000

    taking inputs from ADC, constructing bitmask

--- keyboard/src/main.cpp
+++ keyboard/src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "../../shared/src/CommunicationActor.h"
-#include "../../shared/src/Actor.h"
+#include "../../shared/src/Constants.h"
 #include <Adafruit_MCP3008.h>
+
 
 DebugActor actor = DebugActor();
 Adafruit_MCP3008 inputAdcWhiteKeys;
 
 const int highThreshold = 512;
-
+const byte fullByte = 255;
 void setup() {
 
     pinMode(LED_BUILTIN, OUTPUT);
@@ -18,16 +19,20 @@ void setup() {
     CommunicationActor::initialise(Instrument::Keyboard, &actor);
 }
 
-void loop() {
-    for(int channel = 0; channel < 8; channel++)
-    {
-        //reading inputs to create bitmask for piano keys
-        inputAdcWhiteKeys.readADC(channel);
-    }
-}
-
 bool readHigh(int reading)
 {
     //test if input is above highThreshold
     return (reading > highThreshold);
 }
+
+void loop() {
+
+    byte inputMask = fullByte;
+
+    for(int channel = 0; channel < 8; channel++)
+    {
+        inputMask = inputMask & byte(readHigh(inputAdcWhiteKeys.readADC(channel)) * pow(2,channel));
+    }
+}
+
+
