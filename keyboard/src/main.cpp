67bb5905810e40f5aa0a7e100df0de0beea0b9b3
lachlanhBath <lh2494@bath.ac.uk>
2023-11-21 12:02:06 +0000

    implemented black keys in a second ADC, could be improved in functionality

--- keyboard/src/main.cpp
+++ keyboard/src/main.cpp
@@ -7,6 +7,7 @@
 
 DebugActor actor = DebugActor();
 Adafruit_MCP3008 inputAdcWhiteKeys;
+Adafruit_MCP3008 inputAdcBlackKeys;
 
 const int highThreshold = 512;
 const byte fullByte = 255;
@@ -14,7 +15,9 @@ void setup() {
 
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
+    //inputs used are not important, just placeholders for now
     inputAdcWhiteKeys.begin(13,11,12,10);
+    inputAdcBlackKeys.begin(9,8,7,6);
     Serial.println("Init");
     CommunicationActor::initialise(Instrument::Keyboard, &actor);
 }
@@ -25,18 +28,19 @@ bool readHigh(int reading)
     return (reading > highThreshold);
 }
 
-byte readWhiteKeys()
+byte readKeys(Adafruit_MCP3008 keys)
 {
     byte inputMask = fullByte;
 
     for(int channel = 0; channel < 8; channel++)
     {
-        inputMask = inputMask & byte(readHigh(inputAdcWhiteKeys.readADC(channel)) * pow(2,channel));
+        inputMask = inputMask & byte(readHigh(keys.readADC(channel)) * pow(2,channel));
     }
 }
 
 void loop() {
-    byte whiteBitMask = readWhiteKeys();
+    byte whiteBitMask = readKeys(inputAdcWhiteKeys);
+    byte blackBitMask = readKeys(inputAdcBlackKeys);
 }
 
 
