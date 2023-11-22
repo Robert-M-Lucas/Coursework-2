17bddb6b61bb3642487276599398f5e59b3a355c
Robert-App-Dev <robert01.appdev@gmail.com>
2023-11-22 16:39:26 +0000

    Added limited playback functionality to keyboard

--- keyboard/src/main.cpp
+++ keyboard/src/main.cpp
@@ -44,33 +44,68 @@ byte readKeys(Adafruit_MCP3008 *keys)
     return inputMask;
 }
 
-// 2 Bytes
+bool recording = false;
 byte whiteBitMask = 0;
 byte blackBitMask = 0;
+
 unsigned long startTime = 0;
 
+bool playback = false;
+
 void loop() {
-    const unsigned long currentTime = millis();
-    const unsigned long ellapsedTime = currentTime - startTime;
+    // TODO: Move functionality to 'shared' where applicable
 
-    const byte newWhiteBitMask = readKeys(&inputAdcWhiteKeys);
-    const byte newBlackBitMask = readKeys(&inputAdcBlackKeys);
-
-    if (ellapsedTime > MAX_NOTE_DURATION_MS || whiteBitMask != newWhiteBitMask || blackBitMask != newBlackBitMask) {
-        // TODO: Implement this functionality in 'shared'
-        byte data[3] = {};
-        data[0] = whiteBitMask; // White keys
-        data[1] = blackBitMask; // Black keys
-        data[2] = static_cast<byte>(ellapsedTime / INSTRUMENT_POLL_INTERVAL); // Duration
-
-        actor.writeData(data, 3);
-
-        whiteBitMask = newWhiteBitMask;
-        blackBitMask = newBlackBitMask;
-        startTime = currentTime;
+    if (playback || actor.getPlayback()) {
+        // If playback is starting
+        if (!playback) {
+            if (actor.readDataAvailable(3)) {
+                byte data[3] = {};
+                actor.readDataAndRemove(data, 3);
+                unsigned long duration_ms = data[2] * INSTRUMENT_POLL_INTERVAL;
+                delay(500); // TODO: Play sounds
+            }
+            // Playback ended and all notes in buffer have been played
+            else if (!actor.getPlayback()) {
+                playback = false;
+            }
+            else {
+                Serial.println("Playback ongoing but no data is available!");
+            }
+        }
     }
+    else if (recording || actor.getRecording()) {
+        const unsigned long currentTime = millis();
+        const unsigned long ellapsedTime = currentTime - startTime;
 
-    delay(INSTRUMENT_POLL_INTERVAL);
+        const byte newWhiteBitMask = readKeys(&inputAdcWhiteKeys);
+        const byte newBlackBitMask = readKeys(&inputAdcBlackKeys);
+
+        // If recording is starting
+        if (!recording) {
+            startTime = currentTime;
+            whiteBitMask = newWhiteBitMask;
+            blackBitMask = newBlackBitMask;
+        }
+        // If not changed, note too long or recording is ending
+        else if (ellapsedTime > MAX_NOTE_DURATION_MS ||
+                whiteBitMask != newWhiteBitMask || blackBitMask != newBlackBitMask ||
+                actor.getRecording() == false) {
+                byte data[3] = {};
+                data[0] = whiteBitMask; // White keys
+                data[1] = blackBitMask; // Black keys
+                data[2] = static_cast<byte>(ellapsedTime / INSTRUMENT_POLL_INTERVAL); // Duration
+
+                actor.writeData(data, 3);
+
+                whiteBitMask = newWhiteBitMask;
+                blackBitMask = newBlackBitMask;
+                startTime = currentTime;
+        }
+
+        delay(INSTRUMENT_POLL_INTERVAL);
+
+        recording = actor.getRecording();
+    }
 }
 
 
