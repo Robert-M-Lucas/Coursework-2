6cf7d2ad5a23d3e76659790c326ef5dd7cc30c13
lachlanhBath <lh2494@bath.ac.uk>
2023-11-27 16:15:30 +0000

    added constants for pins for ADCs, created constants.h

---
+++ keyboard/src/constants.h
@@ -0,0 +1,10 @@
+//
+// Created by lachl on 27/11/2023.
+//
+
+#ifndef KEYBOARD_CONSTANTS_H
+#define KEYBOARD_CONSTANTS_H
+
+constexpr unsigned int whiteAdcPins[4] = {10,11,12,13};
+constexpr unsigned int blackAdcPins[4] = {6,7,8,9};
+#endif //KEYBOARD_CONSTANTS_H
