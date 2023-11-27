2bdee76909a5537e352e76810b864adc8f7e430b
lachlanhBath <lh2494@bath.ac.uk>
2023-11-27 16:21:13 +0000

    ammended naming style on constants

--- keyboard/src/constants.h
+++ keyboard/src/constants.h
@@ -5,6 +5,6 @@
 #ifndef KEYBOARD_CONSTANTS_H
 #define KEYBOARD_CONSTANTS_H
 
-constexpr unsigned int whiteAdcPins[4] = {10,11,12,13};
-constexpr unsigned int blackAdcPins[4] = {6,7,8,9};
+constexpr unsigned int WHITE_ADC_PINS[4] = {10,11,12,13};
+constexpr unsigned int BLACK_ADC_PINS[4] = {6,7,8,13};
 #endif //KEYBOARD_CONSTANTS_H
