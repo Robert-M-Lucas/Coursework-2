7c7d68b8b10c3c8185fd9bdf5fcf7699c09cc6cb
robert-m-lucas <noreply@example.com>
2023-11-30 11:08:42 +0000

    Fixed constants

--- shared/src/Constants.h
+++ shared/src/Constants.h
@@ -11,16 +11,16 @@ typedef uint8_t pin;
 constexpr unsigned int BUFFER_SIZE = 256;
 
 /// Delay between consecutive transfers/requests to actors to prevent an interrupt during an interrupt
-constexpr unsigned int TRANSMISSION_DELAY     = 5;
+constexpr unsigned int TRANSMISSION_DELAY = 5;
 
 constexpr unsigned int INSTRUMENT_POLL_INTERVAL = 30;
 
 /// Debug value
-constexpr unsigned int MAX_INSTRUMENTS        = 8;
+constexpr unsigned int MAX_INSTRUMENTS = 8;
 
-constexpr unsigned long MAX_NOTE_DURATION_MS = 60 * 256;
+constexpr unsigned long MAX_NOTE_DURATION_MS = INSTRUMENT_POLL_INTERVAL * 256;
 
 /// serial communications standardised baud rate
-constexpr unsigned int SERIAL_BAUD_RATE               = 9600;
+constexpr unsigned int SERIAL_BAUD_RATE = 9600;
 
 #endif //CONSTANTS_H
