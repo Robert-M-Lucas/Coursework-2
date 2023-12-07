fb35bd2ab2d00580cb544abf67186614eaf3543f
lachlanhBath <lh2494@bath.ac.uk>
2023-12-07 13:42:44 +0000

    octaves added to constants.h

--- keyboard/src/constants.h
+++ keyboard/src/constants.h
@@ -9,4 +9,7 @@ constexpr unsigned int WHITE_ADC_PINS[4] = {10,11,12,13};
 constexpr unsigned int BLACK_ADC_PINS[4] = {6,7,8,13};
 
 constexpr unsigned int SPEAKER_PINS[3] = {3,4,5};
+
+constexpr unsigned int OCTAVE[8] = {261,293,329,349,392,440,494};
+constexpr unsigned int SHARPS[5] = {277,311,349,415,466};
 #endif //KEYBOARD_CONSTANTS_H
